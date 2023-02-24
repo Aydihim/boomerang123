1404d8ba4faf010567010dc8df1017421359a474
Dimitri Sukhoff <sukhovdk@gmail.com>
2023-02-24 15:46:54 +0300

    Hero done!

--- src/game-models/Hero.js
+++ src/game-models/Hero.js
@@ -1,19 +1,35 @@
 // Наш герой.
+const clc = require('cli-color');
+const Boomerang = require("./Boomerang");
 
 class Hero {
   constructor({ position }) {
-    this.skin = '🤠'; // можете использовать любые emoji '💃'
-    this.position = position;
+    this.skin = ':женщина_играет_в_гандбол::оттенок-кожи-3:'; // можете использовать любые emoji
+    this.posH = posH ?? 1;
+    this.posV = 1;
+    this.boomerang = new Boomerang();
   }
 
   moveLeft() {
     // Идём влево.
-    this.position -= 1;
+    this.posH -= 1;
   }
 
   moveRight() {
     // Идём вправо.
-    this.position += 1;
+    this.posH += 1;
+  }
+
+  moveUp() {
+    if (this.posV > 0) {
+      this.posV -= 1;
+    }
+  }
+
+  moveDown() {
+    if (this.posV < 4) {
+      this.posV += 1;
+    }
   }
 
   attack() {
@@ -22,8 +38,8 @@ class Hero {
   }
 
   die() {
-    this.skin = '💀';
-    console.log('YOU ARE DEAD!💀');
+    this.skin = '🤰';
+    console.log(clc.red('YOU GAINED EXTRA KILOS!🤰'));
     process.exit();
   }
 }
