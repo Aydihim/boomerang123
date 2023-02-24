1fb78181f18c48381acc9b4d956bd5e75e862036
Max Romanov <maxromanov@MacBook-Pro-Maksim.local>
2023-02-24 15:53:48 +0300

    Enemy and keyboard done

--- src/keyboard.js
+++ src/keyboard.js
@@ -3,30 +3,56 @@
 // Главное не используй всё вместе!
 
 const keypress = require('keypress');
-
+const player = require('play-sound')(opts = {})
+const Hero = require('./game-models/Hero');
 // Управление.
 // Настроим соответствия нажатий на клавиши и действий в игре.
 
 const keyboard = {
-  q: () => console.log('q'),
+  a: () => console.log('a'),
   w: () => console.log('w'),
+  s: () => console.log('s'),
+  d: () => console.log('d'),
   e: () => console.log('e'),
-  r: () => console.log('r'),
-  t: () => console.log('t'),
-  y: () => console.log('y'),
+  Space: () => console.log('Space'),
 };
 
 // Какая-то функция.
 
-function runInteractiveConsole() {
+const keyboardFunction = function runInteractiveConsole() {
   keypress(process.stdin);
   process.stdin.on('keypress', (ch, key) => {
     if (key) {
       // Вызывает команду, соответствующую нажатой кнопке.
-      if (key.name in keyboard) {
-        keyboard[key.name]();
+      if (key.name === 'd') {
+        hero.moveRight();
       }
-      // Прерывание программы.
+
+      if (key.name === 'a') {
+        hero.moveLeft();
+      }
+
+      if (key.name === 'Space') {
+        hero.boomerang.moveRight(hero);
+      }
+// нужно добавить в героях функции для движения вверх и вниз
+      if (key.name === 'w' && hero.position1 !== 0) {
+        hero.moveUp();
+      }
+
+      if (key.name === 's' && hero.position1 !== 2) {
+        hero.moveDown();
+      }
+
+      if (key.name === 'e') {
+        hero.boomerang.moveRight(hero);
+
+        player.play('./src/sounds/congratulations.wav', (err) => {
+          if (err) throw err;
+        });
+      }
+      // Прерывание программы. 
+      // (может даже эту часть нужно закомментировать)
       if (key.ctrl && key.name === 'c') {
         process.exit();
       }
@@ -38,3 +64,8 @@ function runInteractiveConsole() {
 // Давай попробуем запустить этот скрипт!
 
 runInteractiveConsole();
+module.exports = keyboardFunction;
+// экспортировать функцию в Game
+
+// keyboardFunction(this.hero, this.enemy);
+// const keyboardFunction = require('./keyboard');
